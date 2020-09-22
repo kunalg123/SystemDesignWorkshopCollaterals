c17c6b84156983806ae7e59f2e58be5713be6276
Kunal Ghosh <kunalpghosh@gmail.com>
2020-09-22 10:43:57 +0530

    cmos inverter files uploaded

---
+++ cmos_inverter.v
@@ -0,0 +1,10 @@
+module cmos_inverter (inv_out, inv_in);
+	output inv_out;
+	input inv_in;
+	supply0 GND; 
+	supply1 PWR;
+
+	pmos(inv_out, PWR, inv_in);
+	nmos(inv_out, GND, inv_in);
+endmodule
+
---
+++ tb_cmos_inverter.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+module tb_cmos_inverter;
+	// Inputs
+	reg inv_in;
+        // Inputs
+	wire inv_out;
+
+	// Instantiate the Unit Under Test (UUT)
+	cmos_inverter uut (
+		.inv_in(inv_in),
+		.inv_out(inv_out)
+	);
+	initial begin
+	$dumpfile("tb_cmos_inverter.vcd");
+	$dumpvars(0,tb_cmos_inverter);
+	// Initialize Inputs
+	inv_in = 0;
+	#8 $finish;
+	end
+always #2 inv_in = ~inv_in;
+endmodule
+
