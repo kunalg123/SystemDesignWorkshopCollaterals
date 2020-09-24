6f5af3d5139a1a4af12452e1d9c33b42f2575351
Kunal Ghosh <kunalpghosh@gmail.com>
2020-09-24 09:03:56 +0530

    32bit ripple carry adder verilog files

---
+++ 32bit_carry_ripple_adder.v
@@ -0,0 +1,38 @@
+`include "1bit_full_adder.v"
+`include "full_adder_sum.v"
+`include "full_adder_cout.v"
+module ripple_carry_adder #(
+	parameter WIDTH = 1 
+	) (
+	 input [WIDTH-1:0] input_a,
+	 input [WIDTH-1:0] input_b,
+	 input carry_in,
+	 output [WIDTH-1:0] final_sum,
+	 output carry_out
+	);
+
+	wire [WIDTH:0] carry;
+	wire [WIDTH-1:0] incr_sum;
+
+	// First full-adder has cin as logic 0
+	assign carry_in = 1'b0;
+	assign carry[0] = carry_in;
+	genvar i;
+	generate 
+	  for (i=0; i<WIDTH; i=i+1)
+	    begin
+	      fa_1bit fa_1bit_inst (
+		      .sum(incr_sum[i]), 
+		      .cout(carry[i+1]), 
+		      .a(input_a[i]), 
+		      .b(input_b[i]), 
+		      .cin(carry[i]) 
+	      );
+	    end
+    	endgenerate  
+        
+	// Concatenate final_sum
+	assign final_sum = {carry[WIDTH-1], incr_sum};
+        assign carry_out = carry[WIDTH];
+	
+endmodule	
---
+++ tb_32bit_carry_ripple_adder.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+module tb_32bit_carry_ripple_adder;
+	parameter N = 32;
+	// Inputs
+	reg [N-1:0] input_a = 0;
+	reg [N-1:0] input_b = 0;
+	reg carry_in = 0;
+	// Outputs
+	wire [N-1:0] final_sum;
+	wire carry_out;
+
+        // Instantiate the Unit Under Test (UUT)
+	ripple_carry_adder #(
+		.WIDTH(32)
+		) uut (
+		.input_a(input_a),
+		.input_b(input_b),
+		.carry_in(carry_in),
+		.final_sum(final_sum),
+		.carry_out(carry_out)
+	);
+
+	initial begin
+	$dumpfile("tb_32bit_carry_ripple_adder.vcd");
+	$dumpvars(0,tb_32bit_carry_ripple_adder);
+	#30 $finish;
+	end
+always #1 input_a = $random; 
+always #2 input_b = $random;
+endmodule
+
