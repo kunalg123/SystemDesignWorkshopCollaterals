c441755fb1779dcea0c12cb97f16891a873b6fe0
Kunal Ghosh <kunalpghosh@gmail.com>
2020-09-25 08:03:43 +0530

    transmission gate verilog files

---
+++ tb_tx_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+module tb_tx_gate;
+        // Inputs
+	reg sel, in;
+        // Outputs
+	wire out;
+
+        // Instantiate the Unit Under Test (UUT)
+	tx_gate uut (
+		.out(out),
+		.sel(sel),
+		.in(in)
+	);	
+	initial begin
+        $dumpfile("tb_tx_gate.vcd");
+        $dumpvars(0,tb_tx_gate);
+        // Initialize Inputs
+	in = 1;
+	sel = 0;
+	#20 $finish;
+	end
+
+always #2 sel = ~sel;
+always #1 in = $random;
+endmodule
+
+
---
+++ tx_gate.v
@@ -0,0 +1,10 @@
+`include "cmos_inverter.v"
+module tx_gate (out, sel, in);
+	output out;
+	input sel, in;
+	wire sel_bar;
+
+	pmos(out, in, sel_bar);
+	nmos(out, in, sel);
+	cmos_inverter inv (sel_bar, sel);
+endmodule
